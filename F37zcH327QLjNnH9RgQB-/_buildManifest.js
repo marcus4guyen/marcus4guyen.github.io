631d484bb9a11e54e22ecaf0c1bcfe089627a08c
marcus4guyen <tuananh6109@gmail.com>
2022-12-04 16:32:46 +0700

    Add portfolio

---
+++ F37zcH327QLjNnH9RgQB-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-f35603f756dfc1fb.js","static/chunks/907-cc923a635705271c.js","static/chunks/pages/index-86515d84035d5ad5.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
